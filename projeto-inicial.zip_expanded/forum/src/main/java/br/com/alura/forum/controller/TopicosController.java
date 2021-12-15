76b5c8c1c1d952ab600757fd1e4674a5bdc26eb5
Daniel Collione <danielcollione@MacBook-Air-de-suporte.local>
2021-12-15 15:42:22 -0300

    continuando

--- projeto-inicial.zip_expanded/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ projeto-inicial.zip_expanded/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,7 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,16 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController //Tells spring not to browse as page in every method
 public class TopicosController {
 	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	//@ResponseBody Tells spring not to browse as page
-	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Duvidas", "Duvida com Java", new Curso("Spring", "Programacao"));
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	public List<TopicoDto> lista(String nomeCurso) {
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
 	}
 	
 }
